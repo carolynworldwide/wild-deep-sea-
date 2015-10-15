800be5222c3f1d143264c7af190ba2cfc32f7fa6
Aaron Filson <aaronfilson@gmail.com>
2015-10-14 17:28:52 -0700

    Got the website clicks to register in the tally function.

--- app-revised.js
+++ app-revised.js
@@ -27,14 +27,17 @@ function randPhoto() {
 
 var pix1 = document.getElementById('pix1');
 var photo1 = document.createElement('img');
-photo1.setAttribute('src', randPhoto().photo);
+var saveTheCreature1 = randPhoto();
+
+photo1.setAttribute('src', saveTheCreature1.photo);
 pix1.appendChild(photo1);
 
 var pix2 = document.getElementById('pix2');
 var photo2 = document.createElement('img');
 
+var saveTheCreature2 = randPhoto();
 
-photo2.setAttribute('src', randPhoto().photo);
+photo2.setAttribute('src', saveTheCreature2.photo);
 while (photo1.src === photo2.src) {
   console.log('same photo, try again!');
   photo2.src = randPhoto().photo;
@@ -42,9 +45,20 @@ while (photo1.src === photo2.src) {
 
 pix2.appendChild(photo2);
 
-// var button1 = document.getElementById('button1');
-// button1.addEventListener('click', tally);
+function tally(science) {
+  console.log('in the tally function!' + science);
+  //do some tallying of votes
+  var whateverwewant = science.currentTarget.parentElement.id;
+  console.log(whateverwewant + ' is the click location...');
+  console.log('this is photo1 ' + photo1);
+  console.log('this is photo2 ' + photo2);
+  console.log('this is the object that was saved for the random photo 1 ' + saveTheCreature1);
+  console.log('this is the object that was saved for the random photo 2 ' + saveTheCreature2);
+  saveTheCreature1.votes +=1;
+  saveTheCreature2.votes +=1;
 
-// var button2 = document.getElementById('button2');
-// button2.addEventListener('click', tally);
+}
+
+photo1.addEventListener('click', tally);
+photo2.addEventListener('click', tally);
 
